610fd5340bcf9e089087a6eb06b94e575435df68
Alicia <arrowsrp@yahoo.com>
2022-03-04 18:53:39 -0800

    commit tree 2a5e85a367f7d1ec75501d574118200ce3867a48

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const[listings, setListings] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/listings')
+    .then((res)=>res.json())
+    .then((data)=>setListings(data))
+  }, []);
+
+  function onDeleteClick(deletedListing){
+    let updatedListings = listings.filter((listing)=>listing.id !== deletedListing.id);
+    setListings(updatedListings);
+  };
+
+  function onSubmitSearch (searchTerm) {
+    let searchResults = listings.filter((listing)=>{return listing.description.toLowerCase().includes(searchTerm.toLowerCase())});
+    setListings(searchResults);
+  };
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSubmitSearch={onSubmitSearch} />
+      <ListingsContainer listings={listings} onDeleteClick={onDeleteClick} />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ onSubmitSearch }) {
+  const[searchBy, setSearchBy]=useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    onSubmitSearch(searchBy);
   }
 
   return (
@@ -12,8 +14,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={searchBy ? searchBy : ""}
+        onChange={(e) => {setSearchBy(e.target.value); console.log(searchBy)}}
       />
       <button type="submit">🔍</button>
     </form>
